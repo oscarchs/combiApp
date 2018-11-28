e12051c6adaa2e2fcfa3b8151c1f719728604392
Oscar Chujutalli <oscarchs@bitbucket.org>
2018-11-28 14:24:53 -0500

    lines

--- app/src/main/java/com/example/oscar/combinerito/MapsActivity.java
+++ app/src/main/java/com/example/oscar/combinerito/MapsActivity.java
@@ -19,6 +19,8 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -29,6 +31,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,7 +41,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     Marker marker;
     LocationListener locationListener;
-
+    LatLng cmfb1 = new LatLng(-16.384235,-71.530413);
+    LatLng umacollo = new LatLng(-16.402261, -71.551456);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -52,6 +56,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_PERMISSION);
         }
+        Log.d("asdasd:",ParseDirections(cmfb1,umacollo));
+        Log.d("asdasdasdasdasdasdasda","jjjj");
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -92,14 +98,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng cmfb1 = new LatLng(-16.384235,-71.530413);
-        LatLng umacollo = new LatLng(-16.402261, -71.551456);
+
         ArrayList points =null;
         points = new ArrayList();
         PolylineOptions lineOptions = null;
         lineOptions = new PolylineOptions();
-        points.add(cmfb1);
-        points.add(umacollo);
+
+        points = (ArrayList) decodePolyLines("z`bcBlbtsLe@lAa@dAcAdCuAdDQ^c@bAc@`A_@`Am@fBcAjCQ^GHEFCF");
+
+    //    points.add(cmfb1);
+     //   points.add(umacollo);
         lineOptions.addAll(points);
         lineOptions.width(12);
         lineOptions.color(Color.RED);
@@ -107,6 +115,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addPolyline(lineOptions);
         mMap.addMarker(new MarkerOptions().position(cmfb1).title("Colegio Militar"));
         mMap.addMarker(new MarkerOptions().position(umacollo).title("Umacollo/U.Católica"));
+
     }
 
     @Override
@@ -114,4 +123,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onStop();
         locationManager.removeUpdates(locationListener);
     }
+
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Sensor enabled
+        String sensor = "sensor=false";
+        String mode = "mode=driving";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + mode;
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters +"&key=AIzaSyD9IgLE8qRJn5he2BxthV2W8x-dwvadq8w";
+        return url;
+    }
+
+    private String ParseDirections(LatLng latitude,LatLng longitude){
+        String DirectionsPath = getDirectionsUrl(latitude,longitude);
+        return DirectionsPath;
+
+    }
+
+    public static List<LatLng> decodePolyLines(String poly){
+        int len = poly.length();
+        int index = 0;
+        List<LatLng> decoded = new ArrayList<LatLng>();
+        int lat = 0;
+        int lng = 0;
+
+        while (index < len){
+            int b;
+            int shift = 0;
+            int result = 0;
+            do{
+                b = poly.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = poly.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            }while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >>1));
+            lng += dlng;
+
+            decoded.add(new LatLng(
+                    lat / 100000d,
+                    lng / 100000d
+            ));
+        }
+        return decoded;
+    }
+
 }
